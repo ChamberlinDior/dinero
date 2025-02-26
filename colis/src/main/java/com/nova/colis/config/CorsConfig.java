a66e7aa847e7fe211b7e5f601f09255cb1fd49f2
ChamberlinDior <chamberlindior@gmail.com>
2025-02-25 19:43:22 -0500

    fjfj

--- colis/src/main/java/com/nova/colis/config/CorsConfig.java
+++ colis/src/main/java/com/nova/colis/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                         "http://localhost:8080",        // Pour les tests locaux (navigateur, iOS Sim)
                         "http://192.168.1.37:8080",       // Pour une IP locale
                         "exp://127.0.0.1:19000",          // Pour Expo Go en développement
-                        "http://3.140.185.230:8087"        // Votre URL publique AWS (ou autre URL de production)
+                        "http://3.15.146.182:8087"        // Votre URL publique AWS (ou autre URL de production)
                         // Vous pouvez ajouter d'autres origines si nécessaire
                     )
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
