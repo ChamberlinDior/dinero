1c3d56273f037ab5c38970c79f7a1918e6d28593
ChamberlinDior <chamberlindior@gmail.com>
2025-02-28 00:06:32 -0500

    dfd

--- colis/src/main/java/com/nova/colis/config/WebSocketConfig.java
+++ colis/src/main/java/com/nova/colis/config/WebSocketConfig.java
@@ -27,7 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         "http://localhost:8080",        // Pour tests locaux
                         "http://192.168.1.37:8080",       // Pour IP locale
                         "exp://127.0.0.1:19000",          // Pour Expo Go en développement
-                        "http://3.15.146.182:8087"        // Pour production (exemple AWS)
+                        "http://3.133.121.116:8087"        // Pour production (exemple AWS)
                 )
                 .withSockJS();
     }
